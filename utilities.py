f91cb6304826cbdcc4e9fef117266d0a007da931
austin-mroz <austin.mroz@gmail.com>
2022-05-06 14:22:36 +0100

    uploaded scripts

---
+++ utilities.py
@@ -0,0 +1,32 @@
+"""
+This module defines general-purpose objects, functions and classes.
+"""
+
+# import the necessary packages
+import pandas as pd
+from ase.atoms import Atoms
+from rdkit import DataStructs
+from rdkit import Chem
+from rdkit.Chem import AllChem, Draw
+from tqdm import tqdm
+from rdkit import SimDivFilters
+import numpy as np
+
+def load_smiles_list(filename):
+    smiles = pd.read_csv(filename, header=None)
+    return smiles # smiles.T
+    
+def gen_rdmols(smiles_list):
+    rdmols = []
+    for index, row in smiles_list.iterrows():
+        rdmol = Chem.MolFromSmiles(row[0].replace("'",""))
+        rdmols.append(rdmol)
+    return rdmols
+
+def tanimoto_sim(fps,ntopick):
+    ds=[]
+    for i in range(1,len(fps)):
+         ds.extend(DataStructs.BulkTanimotoSimilarity(fps[i],fps[:i],returnDistance=True))
+    mmp =SimDivFilters.MaxMinPicker()
+    ids=mmp.Pick(np.array(ds),len(fps),ntopick)
+    return ids
