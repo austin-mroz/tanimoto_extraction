c25b554e6c7866939ff95a011cf14246c5a80441
austin-mroz <austin.mroz@gmail.com>
2022-05-06 14:40:00 +0100

    added an example

---
+++ example/extract_by_tanimoto.py
@@ -0,0 +1,32 @@
+from rdkit import Chem
+from rdkit.Chem import Draw
+from rdkit.Chem.rdMolDescriptors import GetMorganFingerprint
+from rdkit.SimDivFilters.rdSimDivPickers import MaxMinPicker
+
+from utilities import load_smiles_list, tanimoto_sim, gen_rdmols
+
+# load your smiles data
+smiles_path = 'example_chem_set.csv'
+smiles_list = load_smiles_list(smiles_path)
+
+# convert your smiles to rdkit molecule objects
+rdmols = gen_rdmols(smiles_list)
+
+# calculate the Morgan fingerprints
+fps = [GetMorganFingerprint(x,3) for x in rdmols] # list of fingerprints
+nfps = len(fps) # total number of fingerprints
+
+# this is the money line -- this function extracts the top num2extract candidates
+num2extract = 23
+dmat_ids=tanimoto_sim(fps,num2extract)
+
+# save subset as an image
+img = Draw.MolsToGridImage([rdmols[x] for x in dmat_ids],molsPerRow=4)
+img.save('extracted_subset.png')
+
+# write subset smiles to a file
+n = open('extracted_subset.txt','w')
+for x in dmat_ids:
+    n.write(Chem.MolToSmiles(rdmols[x])+'\n')
+n.close()
+
